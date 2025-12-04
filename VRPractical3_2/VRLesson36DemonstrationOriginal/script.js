69e839c4a8fc135e08369a2edb7812c068519928
PorchettaEP <eporchetta@schools.nyc.gov>
2025-12-04 13:32:55 +0000

    qqq

---
+++ VRPractical3_2/VRLesson36DemonstrationOriginal/script.js
@@ -0,0 +1,10 @@
+let rnd = (l,u) => Math.random() * (u-l) + l
+let scene, camera;
+
+window.onload = function(){
+  scene = document.querySelector("a-scene");
+  camera = document.querySelector("a-camera");
+
+  
+
+}
