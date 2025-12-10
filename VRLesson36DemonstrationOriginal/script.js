68c38b2a90004ccdb20fccc4b345908c5e1275c7
PorchettaEP <eporchetta@schools.nyc.gov>
2025-12-10 15:48:22 +0000

    1232

--- VRLesson36DemonstrationOriginal/script.js
+++ VRLesson36DemonstrationOriginal/script.js
@@ -1,10 +1,35 @@
 let rnd = (l,u) => Math.random() * (u-l) + l
 let scene, camera;
 
-window.onload = function(){
+window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   camera = document.querySelector("a-camera");
 
-  
 
-}
+
+});
+
+window.addEventListener("click",function(){
+ let x = camera.object3D.position.x;
+ let z = camera.object3D.position.z;
+ //new Box(x,1,z-2);
+  
+});
+
+window.addEventListener("keydown",function(e){
+  //console.log(e.key);
+
+ let x = camera.object3D.position.x;
+ let z = camera.object3D.position.z;
+ if(e.key == 's'){
+  new Sphere(x,1, z-2);
+ }
+ else if (e.key=='b'){
+   new Box(x,1,z-2);
+
+ }
+ else if(e.key=='c'){
+  new Cone(x,1,z-2);
+ }
+  
+});
